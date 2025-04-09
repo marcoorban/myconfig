fff60327b9d045771b9545349a611d747e420dbe
Marco Orban <marco.orban@gmail.com>
2025-04-09 15:06:03 +0800

    Added line to remove previous config files and corrected folder path so that it replaces the right things when it is cloned from github

--- addlinks.sh
+++ addlinks.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# First make sure to delete all the old configuration files
+rm -rf ~/.tmux.conf ~/.vimrc ~/.vim ~/.bashrc
+
 # Create symbolic links in home to follow this folder's dotfiles
 # Tmux and vim config files
-ln -s ~/.myconfig/.tmux.conf ~/.tmux.conf
-ln -s ~/.myconfig/.vimrc ~/.vimrc
-ln -s ~/.myconfig/.vim ~/.vim
+ln -s ~/myconfig/.tmux.conf ~/.tmux.conf
+ln -s ~/myconfig/.vimrc ~/.vimrc
+ln -s ~/myconfig/.vim ~/.vim
 ## Don't forget to run :PlugInstall in vim to install the plugins that are copied.
 
 # Bashrc config files (for those systems where installing zsh is hard, i.e.
 # systems where I don't have admin privileges
-ln -s ~/.myconfig/.bashrc ~/.bashrc
+ln -s ~/myconfig/.bashrc ~/.bashrc
